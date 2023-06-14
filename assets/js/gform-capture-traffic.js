009792db29957d8976fac0c8d14d62a6ac1f112a
hbhalodia <hit.bhalodia@rtcamp.com>
2023-06-14 09:29:04 +0530

    Fix coding style as per WordPress and added search engine duckduckgo for organic search

--- assets/js/gform-capture-traffic.js
+++ assets/js/gform-capture-traffic.js
@@ -11,47 +11,47 @@
  */
 function setHiddenFieldTrafficSources() {
 
-	const utmCampaignHiddenField = document.querySelectorAll('.utm_campaign input');
-	const utmSourceHiddenField = document.querySelectorAll('.utm_source input');
-	const utmMediumHiddenField = document.querySelectorAll('.utm_medium input');
-	const utmTermHiddenField = document.querySelectorAll('.utm_term input');
+	const utmCampaignHiddenField = document.querySelectorAll( '.utm_campaign input' );
+	const utmSourceHiddenField = document.querySelectorAll( '.utm_source input' );
+	const utmMediumHiddenField = document.querySelectorAll('.utm_medium input' );
+	const utmTermHiddenField = document.querySelectorAll( '.utm_term input' );
 
 	// Check the Query Params for UTM Parameter and add it on global const object.
 	let getTrafficSource = getQueryParamsTrafficSourceData();
 
-	if (0 === Object.keys(getTrafficSource).length) {
+	if ( 0 === Object.keys( getTrafficSource ).length ) {
 
-		getTrafficSource = getCookie('GFOrganicDirectTrafficSouce');
+		getTrafficSource = getCookie( 'GFOrganicDirectTrafficSouce' );
 
-		if (null === getTrafficSource || 'undefined' === typeof getTrafficSource) {
+		if ( null === getTrafficSource || 'undefined' === typeof getTrafficSource ) {
 
 			getTrafficSource = getTrafficSourceData();
 
 			// Store value in Cookies.
-			createCookie('GFOrganicDirectTrafficSouce', JSON.stringify(getTrafficSource), 30);
+			createCookie( 'GFOrganicDirectTrafficSouce', JSON.stringify( getTrafficSource ), 30 );
 		} else {
-			getTrafficSource = JSON.parse(getTrafficSource);
+			getTrafficSource = JSON.parse( getTrafficSource );
 		}
 	} else {
 		// Store value in Cookies.
-		createCookie('GFOrganicDirectTrafficSouce', JSON.stringify(getTrafficSource), 30);
+		createCookie( 'GFOrganicDirectTrafficSouce', JSON.stringify( getTrafficSource ), 30 );
 	}
 
 	// Set the Hidden Fields value based on cookie or first time visit within cookie expiration.
-	if (0 < utmCampaignHiddenField.length) {
-		setHiddenFieldsValue(utmCampaignHiddenField, getTrafficSource['utm_campaign']);
+	if ( 0 < utmCampaignHiddenField.length ) {
+		setHiddenFieldsValue( utmCampaignHiddenField, getTrafficSource['utm_campaign'] );
 	}
 
-	if (0 < utmSourceHiddenField.length) {
-		setHiddenFieldsValue(utmSourceHiddenField, getTrafficSource['utm_source']);
+	if ( 0 < utmSourceHiddenField.length ) {
+		setHiddenFieldsValue( utmSourceHiddenField, getTrafficSource['utm_source'] );
 	}
 
-	if (0 < utmMediumHiddenField.length) {
-		setHiddenFieldsValue(utmMediumHiddenField, getTrafficSource['utm_medium']);
+	if ( 0 < utmMediumHiddenField.length ) {
+		setHiddenFieldsValue( utmMediumHiddenField, getTrafficSource['utm_medium'] );
 	}
 
-	if (0 < utmTermHiddenField) {
-		setHiddenFieldsValue(utmTermHiddenField, getTrafficSource['utm_term']);
+	if ( 0 < utmTermHiddenField ) {
+		setHiddenFieldsValue( utmTermHiddenField, getTrafficSource['utm_term'] );
 	}
 }
 
@@ -62,14 +62,14 @@ function setHiddenFieldTrafficSources() {
  * @param {string} trafficValue   Value to be set in Hidden fields.
  * @returns void
  */
-function setHiddenFieldsValue(hiddenFields, trafficValue) {
+function setHiddenFieldsValue( hiddenFields, trafficValue ) {
 
-	if (!trafficValue) {
+	if ( ! trafficValue ) {
 		return;
 	}
-	hiddenFields.forEach(hiddenField => {
+	hiddenFields.forEach( hiddenField => {
 		hiddenField.value = trafficValue;
-	});
+	} );
 }
 
 /**
@@ -80,10 +80,10 @@ function setHiddenFieldsValue(hiddenFields, trafficValue) {
 function getQueryParamsTrafficSourceData() {
 
 	const returnObject = {};
-	const urlParams = new URLSearchParams(window.location.search); // phpcs:ignore WordPressVIPMinimum.JS.Window.location
+	const urlParams = new URLSearchParams( window.location.search ); // phpcs:ignore WordPressVIPMinimum.JS.Window.location
 
-	for (const [key, value] of urlParams.entries()) {
-		switch (key) {
+	for ( const [ key, value ] of urlParams.entries() ) {
+		switch ( key ) {
 			case 'utm_campaign':
 				returnObject['utm_campaign'] = value;
 				break;
@@ -112,13 +112,13 @@ function getTrafficSourceData() {
 
 	let returnObject = {};
 
-	if ('' === referrer) {
+	if ( '' === referrer ) {
 		returnObject['utm_campaign'] = '';
-		returnObject['utm_source'] = '';
+		returnObject['utm_source'] = 'none';
 		returnObject['utm_medium'] = 'direct';
 		returnObject['utm_term'] = '';
 	} else {
-		returnObject = getSourceAndMediumForOrganicTraffic(referrer);
+		returnObject = getSourceAndMediumForOrganicTraffic( referrer );
 	}
 
 	return returnObject;
@@ -126,10 +126,11 @@ function getTrafficSourceData() {
 
 // We can add more search engine here and directly we can add search engine.
 const referrerEngineObject = {
-	'https://www.google.com/': 'google.com',
-	'https://www.bing.com/': 'bing.com',
-	'https://search.yahoo.com/': 'yahoo.com',
-	'https://yandex.com/': 'yandex.com',
+	'https://www.google.com/': 'google',
+	'https://www.bing.com/': 'bing',
+	'https://search.yahoo.com/': 'yahoo',
+	'https://yandex.com/': 'yandex',
+	'https://duckduckgo.com/': 'duckduckgo',
 };
 
 /**
@@ -139,17 +140,17 @@ const referrerEngineObject = {
  *
  * @returns object.
  */
-function getSourceAndMediumForOrganicTraffic(referrer) {
+function getSourceAndMediumForOrganicTraffic( referrer ) {
 
 	let websiteRef = false;
 
-	if (referrer.includes(window.location.hostname)) { // phpcs:ignore WordPressVIPMinimum.JS.Window.location
+	if ( referrer.includes( window.location.hostname ) ) { // phpcs:ignore WordPressVIPMinimum.JS.Window.location
 		websiteRef = true;
 	}
 
 	const returnObject = {
 		'utm_campaign': '',
-		'utm_source': websiteRef ? 'website' : (referrerEngineObject.hasOwnProperty(referrer) ? referrerEngineObject[referrer] : 'other'),
+		'utm_source': websiteRef ? 'website' : ( referrerEngineObject.hasOwnProperty( referrer ) ? referrerEngineObject[ referrer ] : 'other' ),
 		'utm_medium': websiteRef ? 'website' : 'organic',
 		'utm_term': '', // Need to research for term keyword search.
 	};
@@ -166,12 +167,12 @@ function getSourceAndMediumForOrganicTraffic(referrer) {
  *
  * @returns void
  */
-function createCookie(name, value, minutes) {
+function createCookie( name, value, minutes ) {
 
 	let expires = '';
-	if (minutes) {
+	if ( minutes ) {
 		let date = new Date();
-		date.setTime(date.getTime() + (minutes * 60 * 1000));
+		date.setTime( date.getTime() + ( minutes * 60 * 1000 ) );
 		expires = '; expires=' + date.toGMTString();
 	}
 	document.cookie = name + '=' + value + expires + '; path=/';
@@ -184,21 +185,21 @@ function createCookie(name, value, minutes) {
  *
  * @returns {string|null}
  */
-function getCookie(name) {
+function getCookie( name ) {
 
 	var nameEQ = name + '=';
-	var ca = document.cookie.split(';');
-	for (let i = 0; i < ca.length; i++) {
-		let c = ca[i];
-		while (' ' === c.charAt(0)) {
-			c = c.substring(1, c.length);
+	var ca = document.cookie.split( ';' );
+	for ( let i = 0; i < ca.length; i++ ) {
+		let c = ca[ i ];
+		while ( ' ' === c.charAt(0) ) {
+			c = c.substring( 1, c.length );
 		}
-		if (0 === c.indexOf(nameEQ)) {
-			return c.substring(nameEQ.length, c.length);
+		if ( 0 === c.indexOf( nameEQ ) ) {
+			return c.substring( nameEQ.length, c.length );
 		}
 	}
 	return null;
 }
 
 // On DomContentLoaded.
-document.addEventListener('DOMContentLoaded', setHiddenFieldTrafficSources);
+document.addEventListener( 'DOMContentLoaded', setHiddenFieldTrafficSources );
